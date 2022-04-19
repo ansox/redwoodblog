98a45074eb5f631b423be85edc7464cab00e4478
Anso <anderson.floriano@gmail.com>
2022-04-19 11:36:59 -0300

    Chapter 1

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,13 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <p>About page</p>
+    </>
+  )
+}
+
+export default AboutPage
